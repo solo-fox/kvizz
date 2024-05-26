faafd1d1b73d92cb6ddb8053c0e22d220df90da6
square-db <celer.inc@outlook.com>
2024-05-26 18:00:47 +0000

    Ready to shine

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/logo.png" sizes="any" />
       <body className={inter.className}>
         <StyledComponentsRegistry>
         <NavBar />
